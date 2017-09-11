6d744a4ad929e2e4232cd823a959889b19a03539
Peter Horton <phorton@softstackfactory.org>
2017-09-11 17:44:28 +0000

    app.module Heroes component path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from '../components/hero-detail/hero-detail.component';
 import { Hero } from '../assets/classes/hero/hero';
-import { HeroesComponent } from '../components/heroes/heroes.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { HeroService } from '../providers/hero.service';
 import { HeroSearchComponent } from '../components/hero-search/hero-search.component';

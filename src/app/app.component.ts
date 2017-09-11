19c43abf0f8cf1c43482265a0f0e65f734142638
Peter Horton <phorton@softstackfactory.org>
2017-09-11 19:10:35 +0000

    fixed app.component.html to be main html page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,6 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    template: `
-    <h1>{{title}}</h1>
-    <nav>
-        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-        <router-outlet></router-outlet>
-    `
 })
 
 export class AppComponent {

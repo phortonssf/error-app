c6e7e14fb78df289c7b0b6c0f6496484e9658d80
Peter Horton <phorton@softstackfactory.org>
2017-09-11 22:31:09 +0000

    hero-detail.component Missing Input import

--- src/components/hero-detail/hero-detail.component.ts
+++ src/components/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
